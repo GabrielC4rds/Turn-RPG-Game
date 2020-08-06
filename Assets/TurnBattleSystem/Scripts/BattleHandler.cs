642afb9b8d16ad7455c92c4c04ece02e2000eb58
Gabriel Cards <43912656+GabrielC4rds@users.noreply.github.com>
2020-08-06 16:12:22 -0300

    started

---
+++ Assets/TurnBattleSystem/Scripts/BattleHandler.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleHandler : MonoBehaviour {
+
+    private static BattleHandler instance;
+    public bool startedBattle = false;
+    public int numPlayers;
+
+    public void StartBattle(){
+        startedBattle = true;
+    }
+
+    public static BattleHandler GetInstance() {
+        return instance;
+    }
+
+
+    [SerializeField] private Transform pfCharacterBattle;
+    public Texture2D playerSpritesheet;
+    public Texture2D enemySpritesheet;
+
+    private CharacterBattle playerCharacterBattle;
+    private CharacterBattle enemyCharacterBattle;
+    private CharacterBattle activeCharacterBattle;
+    private State state;
+    public Transform characterTransform;
+
+    private enum State {
+        WaitingForPlayer,
+        Busy,
+    }
+
+    private void Awake() {
+        instance = this;
+    }
+
+    private void Start() {
+        playerCharacterBattle = SpawnCharacter(true);
+        enemyCharacterBattle = SpawnCharacter(false);
+
+        SetActiveCharacterBattle(playerCharacterBattle);
+        state = State.WaitingForPlayer;
+    }
+
+    private void Update() {
+        if (state == State.WaitingForPlayer) {
+            if (startedBattle) {
+                state = State.Busy;
+                playerCharacterBattle.Attack(enemyCharacterBattle, () => {
+                    ChooseNextActiveCharacter();
+                });
+            }
+        }
+    }
+
+    
+
+    private CharacterBattle SpawnCharacter(bool isPlayerTeam) {
+        Vector3 position;
+        //  = new Vector3(-50, 0);
+        // while(numPlayers > 0){
+        if (isPlayerTeam) {
+            position = new Vector3(-50, 0);
+        } else {
+            position = new Vector3(+50, 0);
+        }
+            
+        
+        Transform characterTransform = Instantiate(pfCharacterBattle, position, Quaternion.identity);
+        CharacterBattle characterBattle = characterTransform.GetComponent<CharacterBattle>();
+        characterBattle.Setup(isPlayerTeam);
+
+        return characterBattle;
+    }
+
+    private void SetActiveCharacterBattle(CharacterBattle characterBattle) {
+        if (activeCharacterBattle != null) {
+            activeCharacterBattle.HideSelectionCircle();
+        }
+
+        activeCharacterBattle = characterBattle;
+        activeCharacterBattle.ShowSelectionCircle();
+    }
+
+    private void ChooseNextActiveCharacter() {
+        if (TestBattleOver()) {
+            return;
+        }
+
+        if (activeCharacterBattle == playerCharacterBattle) {
+            SetActiveCharacterBattle(enemyCharacterBattle);
+            state = State.Busy;
+            
+            enemyCharacterBattle.Attack(playerCharacterBattle, () => {
+                ChooseNextActiveCharacter();
+            });
+        } else {
+            SetActiveCharacterBattle(playerCharacterBattle);
+            state = State.WaitingForPlayer;
+        }
+    }
+
+    private bool TestBattleOver() {
+        if (playerCharacterBattle.IsDead()) {
+            // Player dead, enemy wins
+            //CodeMonkey.CMDebug.TextPopupMouse("Enemy Wins!");
+            BattleOverWindow.Show_Static("Enemy Wins!");
+            return true;
+        }
+        if (enemyCharacterBattle.IsDead()) {
+            // Enemy dead, player wins
+            //CodeMonkey.CMDebug.TextPopupMouse("Player Wins!");
+            BattleOverWindow.Show_Static("Player Wins!");
+            return true;
+        }
+
+        return false;
+    }
+}
