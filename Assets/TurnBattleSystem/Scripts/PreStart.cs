471267a3e70874d9b06c65907fde8b0162784b88
Gabriel Cards <43912656+GabrielC4rds@users.noreply.github.com>
2020-08-07 14:13:46 -0300

    finalized

--- Assets/TurnBattleSystem/Scripts/PreStart.cs
+++ Assets/TurnBattleSystem/Scripts/PreStart.cs
@@ -5,29 +5,54 @@ using UnityEngine;
 public class PreStart : MonoBehaviour
 {
 
-    public GameObject player1;
-    public GameObject player2;
-    public GameObject enemy1;
-
     public Transform playerPos1;
     public Transform playerPos2;
     public Transform enemyPos1;
+    public GameObject[] playerCustom;
+    public GameObject[] enemyCustom;
 
 
+    public int numPlayers;
+    public int numEnemies;
+
+    //preciso puxar um player ramdom
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(enemyPos1.rotation.eulerAngles.y);
-        
-        Instantiate(player1, new Vector2(playerPos1.position.x, playerPos1.position.y) ,  Quaternion.Euler(0, playerPos1.rotation.eulerAngles.y,0));
-        Instantiate(player2, new Vector2(playerPos2.position.x, playerPos2.position.y) , Quaternion.Euler(0, playerPos2.rotation.eulerAngles.y,0));
-        Instantiate(enemy1, new Vector2(enemyPos1.position.x, enemyPos1.position.y) , Quaternion.Euler(0, enemyPos1.rotation.eulerAngles.y,0));
+      
+        InstantiateChars();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+           
+    }
+
+    public void InstantiateChars() {
+         int numPlayersArray = 0;
+         int numEnemiesArray = 0;
+            int numPCount = 1;
+            int numECount = 1;
+
+        while(numEnemies > 0){
+          GameObject enemyCustomPos = GameObject.FindGameObjectWithTag("enemyPos"+ numECount);
+
+          Instantiate(enemyCustom[numEnemiesArray], new Vector2(enemyCustomPos.transform.position.x, enemyCustomPos.transform.position.y) ,  Quaternion.Euler(0, enemyCustomPos.transform.rotation.eulerAngles.y,0));
+          numECount++;
+          numEnemiesArray++;
+          numEnemies--;
+        }
+
+        while(numPlayers > 0){
+          GameObject playerCustomPos = GameObject.FindGameObjectWithTag("playerPos"+ numPCount);
+
+          Instantiate(playerCustom[numPlayersArray], new Vector2(playerCustomPos.transform.position.x, playerCustomPos.transform.position.y) ,  Quaternion.Euler(0, playerCustomPos.transform.rotation.eulerAngles.y,0));
+          numPCount++;
+          numPlayersArray++;
+          numPlayers--;
+        }
+
     }
 }
